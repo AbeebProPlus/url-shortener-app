4e062a5428a0b391e0992fdfba19226aa339b329
AbeebProPlus <68737847+AbeebProPlus@users.noreply.github.com>
2023-01-12 17:11:40 +0100

    Update UrlController.java

--- src/main/java/urlShortener/controllers/UrlController.java
+++ src/main/java/urlShortener/controllers/UrlController.java
@@ -2,6 +2,7 @@ package urlShortener.controllers;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import urlShortener.dto.requests.ShortenUrlRequest;
@@ -16,8 +17,8 @@ public class UrlController {
     @Autowired
     private UrlService urlService;
     @PostMapping("/post_url")
-    public ShortenUrlResponse shortenUrl(@RequestBody ShortenUrlRequest shortenUrlRequest){
-        return urlService.shortenUrl(shortenUrlRequest);
+    public ResponseEntity<?> shortenUrl(@RequestBody ShortenUrlRequest shortenUrlRequest){
+        return new ResponseEntity<>(urlService.shortenUrl(shortenUrlRequest), HttpStatus.OK);
     }
     @GetMapping("/url/{shortenedUrl}")
     public void getOriginalUrl(@PathVariable String shortenedUrl,  HttpServletResponse response) throws IOException {
